75ebc589bedff41726a185f367d1fb760198ada6
Unknown <krys.nagorny@gmail.com>
2018-06-26 00:43:58 +0200

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {fade} from "./animations";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,25 @@ import {fade} from "./animations";
   animations: fade
 
 })
-export class AppComponent {
-  title = 'app';
-  name = 'Angular 5';
+export class AppComponent implements OnInit {
   choice = 2;
   state = 'in';
   counter = 0;
   enableAnimation = false;
   imageSource = '';
   imgSrc1 = 'https://img-0.journaldunet.com/fHxo62BTNKztTbwMO8DHZ06ou10=/1280x/smart/3834e8aee43e4ba58e7e05219842cb7b/ccmcms-jdn/10226772.png';
-
   imgSrc2 = 'https://phototrend.fr/wp-content/uploads/2016/03/affinity-on-windows.jpg';
+  constructor(private router: Router) {
 
+  }
   ngOnInit() {
+    // scroll to top
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
     this.imageSource = this.imgSrc1;
   }
 
@@ -29,6 +36,7 @@ export class AppComponent {
     this.counter = 0;
     this.toggleState();
   }
+
 
   toggleImg() {
     if (this.choice === 1) {
