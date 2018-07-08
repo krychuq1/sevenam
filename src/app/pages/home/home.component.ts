e05e3d6ed2b6f05765633a0ab8a758617a8d4c24
Unknown <krys.nagorny@gmail.com>
2018-07-08 21:23:24 +0200

    + last build

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,6 +20,8 @@ export class HomeComponent {
   isLoading:boolean;
   imgUrl: string;
   animationBackground: string;
+  maxHeight: string;
+  backgroundImageCustom: string;
   constructor(private burgerService: BurgerService, private http: HttpClient,
               private loadingService: LoadingService){
     this.isLoading = true;
@@ -66,9 +68,18 @@ export class HomeComponent {
   }
   public imgLoaded(imgObj){
     this.urlArray.push(imgObj);
-    if(this.urlArray.length === 6){
+    if(this.urlArray.length === 1){
       this.imgUrl = this.urlArray[0]['imgUrl'];
       this.animationBackground = this.urlArray[0]['background'];
+      this.maxHeight = this.urlArray[0]['maxHeight'];
+      if(this.urlArray[0]['backgroundImage']){
+        this.backgroundImageCustom = this.urlArray[0]['backgroundImage'];
+        // this.backgroundImageCustom = true;
+
+      }else{
+        this.backgroundImageCustom = undefined;
+      }
+      console.log(this.backgroundImageCustom);
       this.isLoading = false;
       this.loadingService.homeLoader.next(true);
       setInterval(()=>{
@@ -82,9 +93,16 @@ export class HomeComponent {
     if(this.state === 'out'){
       this.imgUrl = this.urlArray[this.counter - 1]['imgUrl'];
       this.animationBackground = this.urlArray[this.counter - 1]['background'];
+      this.maxHeight = this.urlArray[this.counter - 1]['maxHeight'];
+      if(this.urlArray[this.counter -1]['backgroundImage']){
+        this.backgroundImageCustom = this.urlArray[this.counter -1]['backgroundImage'];
+      }else{
+        this.backgroundImageCustom = undefined;
+      }
+      console.log(this.backgroundImageCustom);
+
       if(this.state === 'out'){
         this.toggleState();
-
       }
     }
   }
