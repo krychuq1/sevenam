b74fec33475ce886481c06aee39a73bc3c6c4b28
Unknown <krys.nagorny@gmail.com>
2018-07-09 23:25:47 +0200

    + last build

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {BurgerService} from "../../services/burger.service";
 import {HttpClient} from "@angular/common/http";
 import {LoadingService} from "../../services/loading.service";
@@ -16,11 +16,13 @@ export class HomeComponent {
   urlArray: Array<any>;
   counter: number;
   state = 'in';
+  isMobile; boolean;
   enableAnimation = false;
   isLoading:boolean;
   imgUrl: string;
   animationBackground: string;
   maxHeight: string;
+  mobileHeight: string;
   backgroundImageCustom: string;
   homeHover: boolean;
   animationBoxHover: boolean;
@@ -32,9 +34,20 @@ export class HomeComponent {
     this.subscribe();
     this.urlArray = [];
     this.counter = 1;
-    // this.animationBoxHover = true;
+    this._checkDevice();
 
+    // this.animationBoxHover = true;
   }
+  @HostListener('window:resize', ['$event'])
+  onResize(event?): void {
+    this._checkDevice();
+  }
+
+  private _checkDevice() {
+    const innerWidth = (window.innerWidth);
+    this.isMobile = (innerWidth < 1024);
+  }
+
   public subscribe(){
     this.burgerService.navigation.subscribe(value => {
       this.navBurger = value;
@@ -74,10 +87,12 @@ export class HomeComponent {
   }
   public imgLoaded(imgObj){
     this.urlArray.push(imgObj);
-    if(this.urlArray.length === 1){
+    if(this.urlArray.length === 10){
       this.imgUrl = this.urlArray[0]['imgUrl'];
       this.animationBackground = this.urlArray[0]['background'];
       this.maxHeight = this.urlArray[0]['maxHeight'];
+      this.mobileHeight = this.urlArray[0]['mobileHeight'];
+
       if(this.urlArray[0]['backgroundImage']){
         this.backgroundImageCustom = this.urlArray[0]['backgroundImage'];
         // this.backgroundImageCustom = true;
@@ -100,6 +115,7 @@ export class HomeComponent {
       this.imgUrl = this.urlArray[this.counter - 1]['imgUrl'];
       this.animationBackground = this.urlArray[this.counter - 1]['background'];
       this.maxHeight = this.urlArray[this.counter - 1]['maxHeight'];
+      this.mobileHeight = this.urlArray[this.counter -1]['mobileHeight'];
       if(this.urlArray[this.counter -1]['backgroundImage']){
         this.backgroundImageCustom = this.urlArray[this.counter -1]['backgroundImage'];
       }else{
