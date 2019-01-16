c3b5a05d07075be74e28ade8b9eb8703d0bb5aed
Unknown <krys.nagorny@gmail.com>
2019-01-16 14:58:16 +0100

    add new url to backend

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 export let mailerUrl: string = 'https://sevenam-mailer.herokuapp.com/api/contactForm';
 export let content: string = 'https://sevenam-translation.herokuapp.com/api/content/sevenam/';
-export let newcontent: string = 'http://localhost:4555/api/content/5c3e3f421fc1ab725f9390a1/';
+export let newcontent: string = 'https://content-beta.insightapi.io/api/content/5c3e3f421fc1ab725f9390a1/';
